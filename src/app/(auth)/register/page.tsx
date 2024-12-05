d3002a6dade056e3a87ff0f000084d4c638396ef
triudangcoding <quoctrieuzxc@gmailcom>
2024-12-05 09:31:31 +0700

    core project

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import Registerform from "@/src/app/(auth)/register/form-register";
+import React from "react";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <Registerform></Registerform>
+      <div></div>
+    </div>
+  );
+}
